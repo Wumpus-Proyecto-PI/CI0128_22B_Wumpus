21d99399e37f2fee8e4f311defb031838fa20d33
DanielEscobar19 <danielonchio@gmail.com>
2022-09-22 21:35:54 -0600

    Modelo de puesto sin beneficios

---
+++ src/PI/PI/Models/PuestoModel.cs
@@ -0,0 +1,24 @@
+namespace PersonalProject.Models
+{
+    public class PuestoModel
+    {
+        // nombre del puesto, no deben haber dos iguales
+        public string Nombre { get; set; }
+
+        // numero de plazas que puede tener un puesto
+        public int Plazas { get; set; }
+
+        // salario neto que se gana en el puesto
+        public decimal SalarioNeto { get; set; }
+
+        // salario bruto es el salario luego de impuestos y otros
+        public decimal SalarioBruto { get; set; }
+
+        // beneficios que tiene el puesto
+        // List<BeneficioModel> Beneficios { get; set; }
+
+        // lista de puestos subordinados de este puesto
+        List<PuestoModel> Subordinados { get; set; }
+
+    }
+}
