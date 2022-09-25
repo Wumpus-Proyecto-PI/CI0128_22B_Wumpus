e1debd8495d8026af8251f87c8c6a6c4a9c1ad5d
DanielEscobar19 <danielonchio@gmail.com>
2022-09-25 00:32:07 -0600

    Se carga puesto y sus beneficios, falta subordinados

--- src/PI/PI/Handlers/EstructuraOrgHandler.cs
+++ src/PI/PI/Handlers/EstructuraOrgHandler.cs
@@ -6,6 +6,8 @@ using System.Data.SqlClient;
 using Microsoft.AspNetCore.Builder;
 
 using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+
 namespace PI.Handlers
 {
     public class EstructuraOrgHandler
@@ -47,6 +49,56 @@ namespace PI.Handlers
                 });
             }
             return negocios;
+        }
+        public List<PuestoModel> ObtenerPuesto(DateOnly fechaAnalisis)
+        {
+            // extraer los puestos
+            // nombrePuesto es la llave primaria de puesto
+            string fecha = fechaAnalisis.ToString("yyyy-mm-dd");
+            string consulta = "SELECT * FROM PUESTO WHERE fechaAnalisis = '" + fecha + "'";
+            // puestos y sus beneficios
+            List<PuestoModel> puestos = new List<PuestoModel>();
+            DataTable tablaResultadoPuestos = CrearTablaConsulta(consulta);
+
+
+            foreach (DataRow columna in tablaResultadoPuestos.Rows)
+            {
+                puestos.Add(
+                new PuestoModel
+                {
+                    Nombre = Convert.ToString(columna["nombre"]),
+                    Plazas = Convert.ToInt16(columna["plazasPorPuesto"]),
+                    SalarioBruto = Convert.ToDecimal(columna["salarioBruto"]),
+                    Beneficios = new List<BeneficioModel>()
+                });
+                
+                PuestoModel puestoAgregado = puestos.Last();
+                
+                puestoAgregado.Beneficios = ObtenerBeneficios(puestoAgregado.Nombre);
+
+            }
+            return puestos;
+        }
+
+        private List<BeneficioModel> ObtenerBeneficios(string nombrePuesto)
+        {
+            List < BeneficioModel > resultadoBeneficios = new List<BeneficioModel>();
+
+            // extraer los beneficios
+            string consulta = "SELECT * FROM BENEFICIO_PUESTO WHERE nombrePuesto='" + nombrePuesto + "'";
+            DataTable tablaResultadoBeneficios = CrearTablaConsulta(consulta);
+            foreach (DataRow beneficio in tablaResultadoBeneficios.Rows)
+            {
+                resultadoBeneficios.Add(new BeneficioModel
+                {
+                    // se requiere hacer consulta a la base
+                    nombreBeneficio = Convert.ToString(beneficio["nombreBeneficio"]),
+                    monto = Convert.ToDecimal(beneficio["monto"]),
+                    plazasPorBeneficio = Convert.ToInt16(beneficio["PlazaPorBeneficio"])
+                });
+            }
+
+            return resultadoBeneficios;
         }
     }
 }
