830f7c5f427ac092e007f67835ee3e0aeeae67a8
FABIAN OROZCO CHAVES <fabian.orozcochaves@ucr.ac.cr>
2022-09-26 13:44:27 -0600

    Cambio el atributo tipo por correo de usuario al modelo del negocio y agrego el modelo del análisis.

---
+++ src/PI/PI/Models/AnalisisModel.cs
@@ -0,0 +1,15 @@
+﻿namespace PI.Models
+{
+    public class AnalisisModel
+    {
+        // Clase anidada
+        public class ConfigAnalisisModel
+        {
+            public int TipoNegocio { set; get; }
+        }
+
+        // Atributos
+        public DateTime FechaCreacion { get; set; }
+        public ConfigAnalisisModel Configuracion { get; set; }
+    }
+}
--- src/PI/PI/Models/NegocioModel.cs
+++ src/PI/PI/Models/NegocioModel.cs
@@ -4,6 +4,8 @@ using System;
 public class NegocioModel
 {
     public string Nombre { get; set; }
-    public int Tipo { get; set; }
+    public string CorreoUsuario { get; set; }
     public int ID { get; set; }
+    public List<AnalisisModel> Analisis { set; get; }
+
 }
