b5588e7974214290655bd328f447ac0603a7063f
FABIAN OROZCO CHAVES <fabian.orozcochaves@ucr.ac.cr>
2022-09-26 13:42:27 -0600

    Cambio el nombre del controlador y agrego método para agregar a la base de datos

---
+++ src/PI/PI/Controllers/NegocioController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PI.Handlers;
+
+namespace PI.Controllers
+{
+    public class NegocioController : Controller
+    {
+        // Retorna una lista con todos los modelos de negocio existentes
+        public IActionResult Index()
+        {
+            NegocioHandler handler = new NegocioHandler();
+            var negocios = handler.ObtenerNegocios();
+            return View(negocios);
+        }
+        public IActionResult ViewAgregarNegocio()
+        {
+            // Retorna el formulario (popup) para ingresar el nombre y tipo de negocio.
+            return View();
+        }
+
+        public IActionResult agregarNegocio_BD(String nombreNegocio, String tipoNegocio)
+        {
+            // Inserta el negocio en la base de datos.
+            NegocioHandler handler = new NegocioHandler();
+            handler.ingresarNegocio(nombreNegocio, tipoNegocio);
+            
+            // Redirecciona a la página donde tiene todos los negocios
+            return RedirectToAction("Negocio");
+        }
+    }
+}
