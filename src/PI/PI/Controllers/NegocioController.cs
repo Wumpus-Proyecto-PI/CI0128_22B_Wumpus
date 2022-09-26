a78764da76b2c6058df0ac7a4e0816a55dd0b30b
FABIAN OROZCO CHAVES <fabian.orozcochaves@ucr.ac.cr>
2022-09-26 13:42:47 -0600

    Cambio nombre de método de view a form

--- src/PI/PI/Controllers/NegocioController.cs
+++ src/PI/PI/Controllers/NegocioController.cs
@@ -16,7 +16,7 @@ namespace PI.Controllers
             var negocios = handler.ObtenerNegocios();
             return View(negocios);
         }
-        public IActionResult ViewAgregarNegocio()
+        public IActionResult FormAgregarNegocio()
         {
             // Retorna el formulario (popup) para ingresar el nombre y tipo de negocio.
             return View();
@@ -29,7 +29,7 @@ namespace PI.Controllers
             handler.ingresarNegocio(nombreNegocio, tipoNegocio);
             
             // Redirecciona a la página donde tiene todos los negocios
-            return RedirectToAction("Negocio");
+            return RedirectToAction("Index");
         }
     }
 }
