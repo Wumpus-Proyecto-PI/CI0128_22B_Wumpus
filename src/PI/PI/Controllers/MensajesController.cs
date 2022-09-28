0abbf61abe7013f625cc9b82c26e7f83a821492c
DanielEscobar19 <danielonchio@gmail.com>
2022-09-27 23:15:27 -0600

    se agrega controlador para mensajes de botnoes en costruccion

---
+++ src/PI/PI/Controllers/MensajesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PI.Controllers
+{
+    public class MensajesController : Controller
+    {
+        public IActionResult BajoConstruccion()
+        {
+            return View();
+        }
+    }
+}
