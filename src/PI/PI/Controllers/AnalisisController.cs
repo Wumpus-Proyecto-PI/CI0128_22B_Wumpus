d2bc85ee34991167a75f9a43c9ab6b2076c19512
DanielEscobar19 <danielonchio@gmail.com>
2022-10-02 22:54:12 -0600

    Se guardan datos y se redirige a ventana de configuracion

--- src/PI/PI/Controllers/AnalisisController.cs
+++ src/PI/PI/Controllers/AnalisisController.cs
@@ -6,18 +6,12 @@ using System.Threading.Tasks;
 using PI.Handlers;
 using PI.Controllers;
 using PI.Models;
+using System.Globalization;
 
 namespace PI.Controllers
 {
     public class AnalisisController : Controller
     {
-/*        public IActionResult Index()
-        {
-            AnalisisHandler handler = new AnalisisHandler();
-            // var tipoAnalisis = handler.ObtenerTipoAnalisis();
-            return View();
-        }*/
-
         public IActionResult Index(string fechaAnalisis)
         {
             AnalisisHandler handler = new AnalisisHandler();
@@ -25,8 +19,39 @@ namespace PI.Controllers
             AnalisisModel analisisActual = handler.ObtenerUnAnalisis(fechaCreacionAnalisis);
             ViewData["NombreNegocio"] = handler.obtenerNombreNegocio(fechaCreacionAnalisis);
             ViewData["TituloPaso"] = "Progreso del análisis";
+            ViewBag.fechaAnalisis = fechaCreacionAnalisis;
             // var tipoAnalisis = handler.ObtenerTipoAnalisis();
             return View(analisisActual);
+        }
+
+        public IActionResult ConfiguracionAnalisis (string fechaAnalisis)
+        {
+            ViewBag.FechaAnalisis = fechaAnalisis;
+            AnalisisHandler analisisHandler = new AnalisisHandler();
+            DateTime fechaCreacionAnalisis = DateTime.ParseExact(fechaAnalisis, "yyyy-MM-dd HH:mm:ss.fff", null);
+            ViewBag.fechaAnalisis = fechaCreacionAnalisis;
+
+            ConfigAnalisisModel configAnalisis = analisisHandler.ObtenerConfigAnalisis(fechaCreacionAnalisis);
+            ViewData["fechaFormateada"] = fechaCreacionAnalisis.ToString("dd/MMM/yyyy - hh:mm tt", CultureInfo.InvariantCulture);
+            ViewData["TituloPaso"] = "Configuración del análisis";
+            return View(configAnalisis);
+        }
+
+
+        public IActionResult GuardarConfiguracion(string fechaAnalisis, decimal porcentajeSS = -1.0m, decimal porcentajePL = -1.0m)
+        {
+            DateTime fechaCreacionAnalisis = DateTime.ParseExact(fechaAnalisis, "yyyy-MM-dd HH:mm:ss.fff", null);
+            ViewBag.fechaAnalisis = fechaCreacionAnalisis;
+            AnalisisHandler analisisHandler = new AnalisisHandler();
+            ConfigAnalisisModel configAnalisis = new ConfigAnalisisModel
+            {
+                fechaAnalisis = fechaCreacionAnalisis,
+                PorcentajePL = porcentajePL,
+                PorcentajeSS = porcentajeSS
+            };
+
+            analisisHandler.ActualizarConfiguracionAnalisis(configAnalisis);
+            return RedirectToAction("ConfiguracionAnalisis", "Analisis", new { fechaAnalisis = fechaCreacionAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") });
         }
     }
 }
