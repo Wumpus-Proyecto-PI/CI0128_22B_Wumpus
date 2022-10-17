ed2e8ea566602e50c29c98174360c0080730b48d
WILMER.ARAYA <wilmer.araya@ecci.ucr.ac.cr>
2022-10-17 12:21:54 -0600

    Creo nueva rama para la funcionalidad del analisis de rentabilidad y agrego controlador

---
+++ src/PI/PI/Controllers/RentabilidadController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PI.Models;
+
+namespace PI.Controllers
+{
+    public class RentabilidadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
