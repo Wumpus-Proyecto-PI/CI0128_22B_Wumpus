c135c023ae49b1b463914871e822cab703226f9f
WILMER.ARAYA <wilmer.araya@ecci.ucr.ac.cr>
2022-10-17 12:30:25 -0600

    Creo modelo de producto

---
+++ src/PI/PI/Models/ProductoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PI.Models
+{
+    public class ProductoModel
+    {
+        public string nombre { get; set; }
+        public string porcentajeVentas { get; set; }
+        public string precio { get; set; }
+    }
+}
