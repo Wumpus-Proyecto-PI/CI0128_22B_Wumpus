c3aeca848372b6b40dc7f4000585f77f78d443c0
WILMER.ARAYA <wilmer.araya@ecci.ucr.ac.cr>
2022-10-17 12:49:47 -0600

    El controlador de Rentabilidad recibe la fecha de analisis y envia una lista de modelo del producto a la vista

--- src/PI/PI/Controllers/RentabilidadController.cs
+++ src/PI/PI/Controllers/RentabilidadController.cs
@@ -5,9 +5,39 @@ namespace PI.Controllers
 {
     public class RentabilidadController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string fecha)
         {
-            return View();
+            DateTime fechaConversion = DateTime.ParseExact(fecha, "yyyy-MM-dd HH:mm:ss.fff", null);
+            ViewBag.fechaAnalisis = fechaConversion;
+            ViewBag.porcentajeVentas = "23";
+            var productos = GetListOfProducts();
+            return View(productos);
+        }
+
+        private List<ProductoModel> GetListOfProducts()
+        {
+            List<ProductoModel> productos = new List<ProductoModel>();
+            productos.Add(new ProductoModel
+            {
+                nombre = "Empanadas de carne",
+                porcentajeVentas = "33%",
+                precio = "250"
+            });
+
+            productos.Add(new ProductoModel
+            {
+                nombre = "Empanadas de pollo",
+                porcentajeVentas = "33%",
+                precio = "250"
+            });
+
+            productos.Add(new ProductoModel
+            {
+                nombre = "Empanadas de queso",
+                porcentajeVentas = "33%",
+                precio = "250"
+            });
+            return productos;
         }
     }
 }
