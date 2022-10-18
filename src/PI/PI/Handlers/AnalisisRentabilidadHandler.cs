78e720b812b20ce4c02aa7e24b87a531d479d536
DanielEscobar19 <danielonchio@gmail.com>
2022-10-17 19:44:26 -0600

    Creacion de handlers vacios

---
+++ src/PI/PI/Handlers/AnalisisRentabilidadHandler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PI.Handlers
+{
+    public class AnalisisRentabilidadHandler : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
