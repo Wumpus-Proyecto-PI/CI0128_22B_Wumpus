9ff64006f641114718eb98cdef5b9d184b9788b0
Chris24091 <christopher.perez.cr@gmail.com>
2022-10-18 10:04:10 -0600

    Agregando el modelo de gasto variable y de componente

---
+++ src/PI/PI/Models/GastoVariableModel.cs
@@ -0,0 +1,21 @@
+﻿namespace PI.Models
+{
+    public class GastoVariableModel
+    {
+        // Nombre del producto/gasto variable 
+        public string Nombre { get; set; }
+
+        // Fecha del analisis al que corresponde 
+        public DateTime FechaAnalisis { get; set; }
+
+        // Lote del producto 
+        public int Lote { get; set; }
+
+        // Porecentaje de ventas del producto 
+        public decimal PorcentajeDeVentas { get; set; } 
+
+        // Precio del producto 
+        public decimal Precio { get; set; }
+
+    }
+}
