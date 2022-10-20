1e6a6e38c49bcbbbe338c07f73365658994c33c2
DanielEscobar19 <danielonchio@gmail.com>
2022-10-19 22:13:22 -0600

    se agregan metodos para calcular margen, margen ponderado y la meta de venta en unidades de un producto

--- src/PI/PI/Services/AnalisisRentabilidadService.cs
+++ src/PI/PI/Services/AnalisisRentabilidadService.cs
@@ -5,6 +5,34 @@ namespace PI.Services
 {
     public class AnalisisRentabilidadService
     {
+        // método para calcular el margen de un producto
+        // Parameter: precio  del producto
+        // Parameter: costoVariable total del producto
+        // Return: el valor del margen del producto
+        public decimal calcularMargen(decimal precio, decimal costoVariable)
+        {
+            return precio - costoVariable;
+        }
 
+        // método para calcular el margen ponderado de un producto
+        // Parameter: porcentajeVentas  del producto
+        // Parameter: margen del producto
+        // Return: el valor del margen ponderado del producto
+        public decimal calcularMargenPonderado(decimal porcentajeVentas, decimal margen)
+        {
+            return porcentajeVentas * margen;
+        }
+
+        // método para calcular la meta de ventas de un producto en unidades
+        // Parameter: porcentajeVentas del producto
+        // Parameter: gastosFijosMensuales totales del negocio
+        // Parameetr: gananciaMensual meta indicada por el usuario
+        // Parameetr: margenPonderado calculado con la funcion @calcularMargen
+        // Return: las unidades meta que a vender
+        public int calcularMetaVentasUnidades(decimal porcentajeVentas, decimal gastosFijosMensuales, decimal gananciaMensual, decimal margenPonderado)
+        {
+            decimal resultado = (porcentajeVentas * (gastosFijosMensuales + gananciaMensual)) / margenPonderado
+            return Convert.ToInt32(resultado);
+        }
     }
 }
