45046f784a930dc41ae8054fed82b8fbeeb3ca80
Chris24091 <christopher.perez.cr@gmail.com>
2022-10-24 18:14:13 -0600

    Corrigiendo string de consulta en eliminar y agregar componente

--- src/PI/PI/Handlers/ComponenteHandler.cs
+++ src/PI/PI/Handlers/ComponenteHandler.cs
@@ -10,9 +10,9 @@ namespace PI.Handlers
         {
             int filasAfectadas = 0;
             string consulta = "EXEC AgregarComponente @nombreComponente='" + componente.Nombre.ToString() + "'" +
-                "@nombreProducto='" + componente.NombreProducto.ToString() + "'@fechaAnalisis='" +componente.FechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") +"'" +
-                "@monto='" + componente.Costo.ToString() + "'@cantidad='" + componente.Cantidad.ToString() + "'" +
-                "@unidad='" + componente.Unidad.ToString()+ "'";
+                ",@nombreProducto='" + componente.NombreProducto.ToString() + "',@fechaAnalisis='" +componente.FechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") +"'" +
+                ",@monto='" + componente.Costo.ToString() + "',@cantidad='" + componente.Cantidad.ToString() + "'" +
+                ",@unidad='" + componente.Unidad.ToString()+ "'";
 
             filasAfectadas = enviarConsulta(consulta);
             return filasAfectadas;
@@ -22,7 +22,7 @@ namespace PI.Handlers
         {
             int filasAfectadas = 0;
             string consulta = "EXEC BorrarComponente @nombreComponente='" + componente.Nombre.ToString() + "'" +
-                "@nombreProducto='" + componente.NombreProducto.ToString() + "'@fechaAnalisis='" + componente.FechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") +"'";
+                ",@nombreProducto='" + componente.NombreProducto.ToString() + "',@fechaAnalisis='" + componente.FechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") +"'";
 
             filasAfectadas = enviarConsulta(consulta);
             return filasAfectadas;
