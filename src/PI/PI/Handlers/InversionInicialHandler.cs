37fe2e503c40d1b1df07f0795b1ff5c709cb2c76
FabiánOrozcoChaves <fabian.orozcochaves@ucr.ac.cr>
2022-10-25 23:13:28 -0600

    Creación de componentes mvc. Creo el handler de inversión inicial e implemento los métodos para obtener, ingresar y eliminar los gastos.

--- src/PI/PI/Handlers/InversionInicialHandler.cs
+++ src/PI/PI/Handlers/InversionInicialHandler.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PI.Models;
+using System.Data;
 
 namespace PI.Handlers
 {
-    public class InversionInicialHandler : Controller
+    public class InversionInicialHandler : Handler
     {
-        public IActionResult Index()
+        public List<GastoInicialModel> ObtenerGastosIniciales(string fechaAnalisis)
         {
-            return View();
+            List<GastoInicialModel> GastosIniciales = new List<GastoInicialModel>();
+
+            string consulta = $"EXEC ObtenerGastosIniciales @fechaAnalisis = '{fechaAnalisis}'";
+
+            DataTable tablaResultado = CrearTablaConsulta(consulta);
+
+            foreach (DataRow row in tablaResultado.Rows)
+            {
+                GastosIniciales.Add(
+                    new GastoInicialModel
+                    {
+                        Nombre = Convert.ToString(row["nombre"]),
+                        Monto = Convert.ToDecimal(row["valor"]),
+                        FechaAnalisis = Convert.ToDateTime(row["fechaAnalisis"]),
+                    }
+                );
+            }
+            return GastosIniciales;
+        }
+
+        public void IngresarGastoInicial(string fechaAnalisis, GastoInicialModel GastoInicial)
+        {
+            string consulta = $"EXEC IngresarGastoInicial @fechaAnalisis = '{fechaAnalisis}', @nombre = '{GastoInicial.Nombre}', @valor = {GastoInicial.Monto}";
+
+            enviarConsultaVoid(consulta);
+        }
+
+        public void EliminarGastoInicial(string fechaAnalisis, string nombreGastoInicial)
+        {
+            string consulta = $"EXEC EliminarGastoInicial @nombre= '{nombreGastoInicial}', @fechaAnalisis='{fechaAnalisis}'";
+
+            enviarConsultaVoid(consulta);
         }
     }
 }
--- src/PI/PI/Models/GastoInicialModel.cs
+++ src/PI/PI/Models/GastoInicialModel.cs
@@ -3,7 +3,9 @@
     // Representa un gasto de la sección "inversión inicial".
     public class GastoInicialModel
     {
-        public string? nombre { get; set; }  // representa el nombre del gasto de la inversión inicial.
-        public decimal monto { get; set; }  // representa el costo monetario del gasto de la inversión inicial.
+        public string? Nombre { get; set; }  // representa el nombre del gasto de la inversión inicial.
+        public decimal Monto { get; set; }  // representa el costo monetario del gasto de la inversión inicial.
+        
+        public DateTime FechaAnalisis { get; set; }  // Fecha del análisis a la que pertenece el gasto inicial.
     }
 }
