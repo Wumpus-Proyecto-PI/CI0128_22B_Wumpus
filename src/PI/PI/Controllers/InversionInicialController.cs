5ea69ef419a2dbb791544ff26b4d05b175ae1ecd
FabiánOrozcoChaves <fabian.orozcochaves@ucr.ac.cr>
2022-10-25 23:12:47 -0600

    Creación de componentes mvc. Creo el controlador de inversión inicial e implemento el método principal de la vista predeterminada.

--- src/PI/PI/Controllers/InversionInicialController.cs
+++ src/PI/PI/Controllers/InversionInicialController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PI.Handlers;
+using PI.Models;
 
 namespace PI.Controllers
 {
     public class InversionInicialController : Controller
     {
-        public IActionResult InversionInicial()
+        public IActionResult InversionInicial(string fechaAnalisis)
         {
-            return View();
+            DateTime fechaCreacionAnalisis = DateTime.ParseExact(fechaAnalisis, "yyyy-MM-dd HH:mm:ss.fff", null);
+
+            // Datos que ocupa la vista
+            ViewBag.fechaAnalisis = fechaCreacionAnalisis;
+            ViewData["Title"] = "Inversión inicial";
+            ViewData["TituloPaso"] = ViewData["Title"];
+            ViewBag.BotonRetorno = "Progreso";
+
+            InversionInicialHandler inversionInicialHandler = new InversionInicialHandler();
+            ViewData["NombreNegocio"] = inversionInicialHandler.obtenerNombreNegocio(fechaCreacionAnalisis);
+            
+            return View(inversionInicialHandler.ObtenerGastosIniciales(fechaAnalisis));
         }
     }
 }
