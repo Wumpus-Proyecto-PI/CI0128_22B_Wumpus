7f0642bb6204a09b4311140be01d6215a96f450a
DanielEscobar19 <danielonchio@gmail.com>
2022-10-27 17:20:35 -0600

    Se crean patron plantilla para determinar pasos. El metodo DeterminarPasoAtivo es el patron plantilla que sobreescriben los hijos y el  metodo DeterminarPasoActivoMaximo es el director que llama al plantilla de los hijos para dterminar el paso maximo al que puede ingresar un usuario.

--- src/PI/PI/Services/PasosProgresoControl.cs
+++ src/PI/PI/Services/PasosProgresoControl.cs
@@ -6,32 +6,66 @@ namespace PI.Services
     public class PasosProgresoControl
     {
         protected int NumeroPaso = 1;
+        private readonly List<PasosProgresoControl> PasosControles = new(); 
 
-
-        public int determinarPasoActivoMaximo(AnalisisModel analisis)
+        private void construirPasos()
         {
+            // se agrega el controlado de cada paso
+            PasosControles.Add(new GastosFijosControl());
+            PasosControles.Add(new GastoVariableControl());
+            PasosControles.Add(new RentabilidadControl());
+            PasosControles.Add(new InversionInicialControl());
+            // aqui se agregan los controladores de los pasos nuevos
+
+        }
+
+        // Brief: metodo director que llama al metodo plantilla de los hijos para determinar el paso maximo al que puede entrar el usuario
+        // Param: recibe el analisis del cual determinar el paso maximo al que puede ingresar el usuario
+        // Return: retorna el indice del paso maximo al que puede ingresar el usuario
+        public int DeterminarPasoActivoMaximo(AnalisisModel analisis)
+        {
+            // construimos instancias de cada paso y se agregan a la lista de pasos
+            construirPasos();
+
+            // se asigna 1 al paso que se retorna. Como minimo se puede entrar al paso de estrutura organizativa.
             int pasoActivoMaximo = this.NumeroPaso;
 
+            // buscamos el primer paso que no este activo porque este no se peude acceder
+            PasosProgresoControl? pasoMaximoAccesible = PasosControles.Find(x => x.DeterminarPasoAtivo(analisis) == false);
+
+            // asiganamos el numero de paso a la variable de retorno si nos nulo el resultado de la busqueda
+            if (pasoMaximoAccesible is not null)
+            {
+                pasoActivoMaximo = pasoMaximoAccesible.NumeroPaso - 1;
+            } else
+            {
+                // si es nulo el resultado de la busqueda indicamos que el maximo paso accesible es el primero
+                pasoActivoMaximo = 1;
+            }
 
             return pasoActivoMaximo;
         }
 
-        protected virtual bool determinarPasoAtivo(AnalisisModel analisis)
+        // metodo plantilla que implementan los hijos para determinar si un paso esta activo
+        // Param: recibe el analisis del cual determinar el paso maximo al que puede ingresar el usuario
+        // Return: verdadero si el paso esta activo y se puede ingresar
+        protected virtual bool DeterminarPasoAtivo(AnalisisModel analisis)
         {
             return false;
         }
     }
-    public class GastosFijosControl: PasosProgresoControl
+
+    public class GastosFijosControl : PasosProgresoControl
     {
         public GastosFijosControl()
         {
-            this.NumeroPaso = 2;
+            base.NumeroPaso = 2;
         }
 
         // Indica si el analisis posee puestos
         // (Retorna un bool que indica si hay puestos o no | Parametros: modelo del analisis que se desea verificar)
         // Se encarga de verificar si existen puestos dentro de un análisis.
-        override protected bool determinarPasoAtivo(AnalisisModel analisis)
+        override protected bool DeterminarPasoAtivo(AnalisisModel analisis)
         {
             bool resultado = false;
             // Se crea instancia del handler
@@ -53,13 +87,13 @@ namespace PI.Services
     {
         public GastoVariableControl()
         {
-            this.NumeroPaso = 3;
+            base.NumeroPaso = 3;
         }
 
         // Indica si el analisis posee gastos fijos
         // (Retorna un bool que indica si hay gastos fijos o no | Parametros: modelo del analisis que se desea verificar)
         // Determina si un análisis contiene gastos fijos.
-        override protected bool determinarPasoAtivo(AnalisisModel analisis)
+        override protected bool DeterminarPasoAtivo(AnalisisModel analisis)
         {
             bool resultado = false;
             // Se crea instancia del handler
@@ -86,11 +120,13 @@ namespace PI.Services
     {
         public RentabilidadControl()
         {
-            this.NumeroPaso = 4;
+            base.NumeroPaso = 4;
         }
 
-
-        override protected bool determinarPasoAtivo(AnalisisModel analisis)
+        // metodo que verifica si el paso de analisis rentabilidad esta disponible o no.
+        // recibe el enalisis del cual determiar si el paso esta disponible
+        // retorna true si el paso esta disponible
+        override protected bool DeterminarPasoAtivo(AnalisisModel analisis)
         {
             bool resultado = false;
 
@@ -110,17 +146,17 @@ namespace PI.Services
     {
         public InversionInicialControl()
         {
-            this.NumeroPaso = 4;
+            base.NumeroPaso = 5;
         }
 
         // método que verifica si es posible que exista una meta de ventas en el análisis de rentabilidad.
         // detalle: sirve para determinar si la tarjeta de la inversión inicial se debe habilitar.
         // se asume que si un producto tiene valores en algunos de sus atributos, la meta de ventas ha sido calculada.
-        override protected bool determinarPasoAtivo(AnalisisModel analisis)
+        override protected bool DeterminarPasoAtivo(AnalisisModel analisis)
         {
             bool resultado = false;
             ProductoHandler productoHandler = new ProductoHandler();
-            List<ProductoModel> productos = productoHandler.obtenerProductos(fechaAnalisis);
+            List<ProductoModel> productos = productoHandler.obtenerProductos(analisis.FechaCreacion);
             for (int actual = 0; actual < productos.Count && resultado == false; ++actual)
             {
                 if (productos[actual].Precio > 0
