d790d648ebb828bc59b0e7c4cbeee2ee8c8b3629
DanielEscobar19 <danielonchio@gmail.com>
2022-10-27 21:57:50 -0600

    Code review Fabián. Realizo las correcciones leves del code review.

--- src/PI/PI/Services/PasosProgresoControl.cs
+++ src/PI/PI/Services/PasosProgresoControl.cs
@@ -6,9 +6,9 @@ namespace PI.Services
     public class PasosProgresoControl
     {
         protected int NumeroPaso = 1;
-        private readonly List<PasosProgresoControl> PasosControles = new(); 
+        private List<PasosProgresoControl> PasosControles = new(); 
 
-        private void construirPasos()
+        private void ConstruirPasos()
         {
             // se agrega el controlado de cada paso
             PasosControles.Add(new GastosFijosControl());
@@ -22,26 +22,28 @@ namespace PI.Services
         // Brief: metodo director que llama al metodo plantilla de los hijos para determinar el paso maximo al que puede entrar el usuario
         // Param: recibe el analisis del cual determinar el paso maximo al que puede ingresar el usuario
         // Return: retorna el indice del paso maximo al que puede ingresar el usuario
-        public int DeterminarPasoActivoMaximo(AnalisisModel analisis)
+        public int EstaActivoMaximo(AnalisisModel analisis)
         {
             // construimos instancias de cada paso y se agregan a la lista de pasos
-            construirPasos();
+            ConstruirPasos();
 
-            // se asigna 1 al paso que se retorna. Como minimo se puede entrar al paso de estrutura organizativa.
+            // se asigna 1 al paso que se retorna. Como minimo se puede entrar al paso de estructura organizativa.
             int pasoActivoMaximo = this.NumeroPaso;
 
-            // buscamos el primer paso que no este activo porque este no se peude acceder
-            PasosProgresoControl? pasoMaximoAccesible = PasosControles.Find(x => x.DeterminarPasoAtivo(analisis) == false);
+            // buscamos el primer paso que no este activo porque este no se puede acceder
+            PasosProgresoControl? pasoMaximoAccesible = PasosControles.Find(x => x.EstaActivo(analisis) == false);
 
-            // asiganamos el numero de paso a la variable de retorno si nos nulo el resultado de la busqueda
+            // si el find retorna no nulo es que no se econctro un paso no accesible
             if (pasoMaximoAccesible is not null)
             {
+                // asignamos el numero de paso a la variable
+                // le restamos porque pasoMaximoAccesible es el paso al que no se puede acceder, su posicion menos 1 es maximo paso que si se peude acceder
                 pasoActivoMaximo = pasoMaximoAccesible.NumeroPaso - 1;
             } else
             {
                 // si es nulo el resultado de la busqueda indicamos que el maximo paso accesible es el ultimo paso que haya en la lista
                 // esto porque si se obtuvo nulo es que no hubo nigun paso no activo en las lista, por lo tanto todos esta activos
-                // le sumamos 1 porque en la lita de pasos no se incluye el paso default que es la estrutura organizativa
+                // le sumamos 1 porque en la lista de pasos no se incluye el paso default que es la estructura organizativa
                 pasoActivoMaximo = PasosControles.Count + 1;
             }
 
@@ -51,8 +53,11 @@ namespace PI.Services
         // metodo plantilla que implementan los hijos para determinar si un paso esta activo
         // Param: recibe el analisis del cual determinar el paso maximo al que puede ingresar el usuario
         // Return: verdadero si el paso esta activo y se puede ingresar
-        protected virtual bool DeterminarPasoAtivo(AnalisisModel analisis)
+        protected virtual bool EstaActivo(AnalisisModel analisis)
         {
+            // nunca se llama este metodo desde una instancia del padre
+            // como es virtual la funcion se debe implementar en el padre, no obstante no se va a usar
+            // por eso su valor de retorno no tiene importancia
             return false;
         }
     }
@@ -67,14 +72,14 @@ namespace PI.Services
         // Indica si el analisis posee puestos
         // (Retorna un bool que indica si hay puestos o no | Parametros: modelo del analisis que se desea verificar)
         // Se encarga de verificar si existen puestos dentro de un análisis.
-        override protected bool DeterminarPasoAtivo(AnalisisModel analisis)
+        override protected bool EstaActivo(AnalisisModel analisis)
         {
             bool resultado = false;
             // Se crea instancia del handler
-            EstructuraOrgHandler estHandler = new EstructuraOrgHandler();
+            EstructuraOrgHandler estructuraOrgHandler = new EstructuraOrgHandler();
 
             // Se obtiene de la base de datos los diferentes puestos del Análisis.
-            List<PuestoModel> puestos = estHandler.ObtenerListaDePuestos(analisis.FechaCreacion);
+            List<PuestoModel> puestos = estructuraOrgHandler.ObtenerListaDePuestos(analisis.FechaCreacion);
 
             // Se determina si la cantidad de puestos que posee es mayor a 0
             if (puestos.Count > 0)
@@ -95,24 +100,20 @@ namespace PI.Services
         // Indica si el analisis posee gastos fijos
         // (Retorna un bool que indica si hay gastos fijos o no | Parametros: modelo del analisis que se desea verificar)
         // Determina si un análisis contiene gastos fijos.
-        override protected bool DeterminarPasoAtivo(AnalisisModel analisis)
+        override protected bool EstaActivo(AnalisisModel analisis)
         {
             bool resultado = false;
-            // Se crea instancia del handler
+            // se crea instancia del handler
             GastoFijoHandler gastosHandler = new GastoFijoHandler();
             
-            // Mediante el handler, se obtiene de la base de datos la cantidad de gastos fijos que contiene un análisis.
+            // mediante el handler, se obtiene de la base de datos la cantidad de gastos fijos que contiene un análisis.
             List<GastoFijoModel> gastosFijos = gastosHandler.ObtenerGastosFijos(analisis.FechaCreacion);
 
-            // Por cada uno de los gastos fijos obtenidos, se verifica si corresponde a uno de los gastos fijos por defecto de los análisis.
-            // Si alguno de los gastos fijos obtenidos es diferente a todos ellos, se determina que si se le han agregado gastos fijos al análisis.
-            for (int i = 0; i < gastosFijos.Count(); i += 1)
+            // se revisa si hay mas de 4 gastos fijos porque siempre existen los gastos de: 
+            // salarios, beneficios, prestaciones laboralos y seguro social
+            if (gastosFijos.Count > 4)
             {
-                if (gastosFijos[i].Nombre != "Seguridad social" && gastosFijos[i].Nombre != "Prestaciones laborales" && gastosFijos[i].Nombre != "Beneficios de empleados" && gastosFijos[i].Nombre != "Salarios netos")
-                {
-                    resultado = true;
-                    break;
-                }
+                resultado = true;
             }
             return resultado;
         }
@@ -128,7 +129,7 @@ namespace PI.Services
         // metodo que verifica si el paso de analisis rentabilidad esta disponible o no.
         // recibe el enalisis del cual determiar si el paso esta disponible
         // retorna true si el paso esta disponible
-        override protected bool DeterminarPasoAtivo(AnalisisModel analisis)
+        override protected bool EstaActivo(AnalisisModel analisis)
         {
             bool resultado = false;
 
@@ -154,7 +155,7 @@ namespace PI.Services
         // método que verifica si es posible que exista una meta de ventas en el análisis de rentabilidad.
         // detalle: sirve para determinar si la tarjeta de la inversión inicial se debe habilitar.
         // se asume que si un producto tiene valores en algunos de sus atributos, la meta de ventas ha sido calculada.
-        override protected bool DeterminarPasoAtivo(AnalisisModel analisis)
+        override protected bool EstaActivo(AnalisisModel analisis)
         {
             bool resultado = false;
             ProductoHandler productoHandler = new ProductoHandler();
