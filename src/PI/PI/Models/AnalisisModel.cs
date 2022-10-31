c2cbfff460058b0ea699961fb46b4e3d5d50fc50
gabriel.bonillarivera <gabriel.bonillarivera@ucr.ac.cr>
2022-10-31 14:34:22 -0600

    Avance manejo de input ganancia mensual

--- src/PI/PI/Models/AnalisisModel.cs
+++ src/PI/PI/Models/AnalisisModel.cs
@@ -1,4 +1,6 @@
-﻿namespace PI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+namespace PI.Models
 {
     public class AnalisisModel
     {
@@ -7,6 +9,9 @@
         public DateTime FechaCreacion { get; set; } // representa la fecha unica de creacion del analisis
         public ConfigAnalisisModel Configuracion { get; set; } = new ConfigAnalisisModel(); // configuracion del analisis (clase)
         public bool Finalizado { get; set; } // Indica si el análisis ha sido finalizado o no.
+
+        [Range(0.0, Double.MaxValue,
+            ErrorMessage = "Ingresó un número muy pequeño o muy grande")]
         public decimal gananciaMensual { get; set; }
         public int estadoAnalisis { get; set; } // Indica si el análisis ha sido finalizado o no. 1: en curso. 2: finalizado.
     }
