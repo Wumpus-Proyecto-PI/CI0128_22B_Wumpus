8d12fb712482cae8112fedc6cdb9ae849c48b86b
DanielEscobar19 <danielonchio@gmail.com>
2022-11-13 23:54:51 -0600

    Scaffolding de identity. Se generan los archivos base de identity para el manejo de usuarios.

--- src/PI/PI/Program.cs
+++ src/PI/PI/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PI.Areas.Identity.Data;
+using PI.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("UserDbApplicationContextConnection") ?? throw new InvalidOperationException("Connection string 'UserDbApplicationContextConnection' not found.");
+
+builder.Services.AddDbContext<UserDbApplicationContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<UserApplication>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<UserDbApplicationContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -19,6 +30,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
