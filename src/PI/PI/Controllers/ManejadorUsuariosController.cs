8918406ec47f0f73709d7a26718781d59691b119
DanielEscobar19 <danielonchio@gmail.com>
2022-11-15 10:30:50 -0600

    Clase padre para evitar accesos restringido. Todos nuestrros controladores heredan de este para bloquear alusuario al tratar de saltrase e login en la app.

---
+++ src/PI/PI/Controllers/ManejadorUsuariosController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PI.Controllers
+{
+    [Microsoft.AspNetCore.Authorization.Authorize]
+    public class ManejadorUsuariosController : Controller
+    {
+    }
+}
