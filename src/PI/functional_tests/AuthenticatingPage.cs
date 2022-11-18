7eb5c8615fd72797ba8820c4cc6c88f02ba563d7
FabiánOrozcoChaves <fabian.orozcochaves@ucr.ac.cr>
2022-11-18 15:11:29 -0600

    Comienzo POM. Agrego la página de autenticación.

---
+++ src/PI/functional_tests/AuthenticatingPage.cs
@@ -0,0 +1,95 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using UnitTestsResources;
+
+namespace PiTests
+{
+    public class AuthenticatingPage
+    {
+        private IWebDriver Driver;
+        private string URL = "https://localhost:7073";
+        private string CorreoPredeterminado = "wumpustest@gmail.com";
+        private string UsuarioRegistroPredeterminado = "wumpusRegisterTest@gmail.com";
+        private string ContraseñaPredeterminada = "wumpus";
+
+
+        private IWebElement EntradaCorreo 
+        { 
+            get 
+            { 
+                return Driver.FindElement(By.Id("Input_Email")); 
+            } 
+        }
+
+        private IWebElement EntradaContraseña
+        {
+            get
+            {
+                return Driver.FindElement(By.Id("Input_Password"));
+            }
+        }
+
+        private IWebElement EntradaConfirmarContraseña
+        {
+            get
+            {
+                return Driver.FindElement(By.Id("Input_ConfirmPassword"));
+            }
+        }
+
+        private IWebElement BotonIniciarSesion
+        {
+            get
+            {
+                return Driver.FindElement(By.Id("login-submit"));
+            }
+        }
+
+        private IWebElement BotonCerrarSesion
+        {
+            get
+            {
+                return Driver.FindElement(By.ClassName("logout-button"));
+            }
+        }
+
+        private IWebElement BotonRegistrarse
+        {
+            get
+            {
+                return Driver.FindElement(By.Id("registerSubmit"));
+            }
+        }
+
+        public AuthenticatingPage(IWebDriver driver) {
+            this.Driver = driver;
+        }
+
+        public void IniciarSesionUsuarioDefault()
+        {
+            this.EntradaCorreo.SendKeys(CorreoPredeterminado);
+            this.EntradaContraseña.SendKeys(ContraseñaPredeterminada);
+            this.BotonIniciarSesion.Click();
+        }
+        public void RegistrarUsuarioPredeterminado ()
+        {
+            this.EntradaCorreo.SendKeys(this.UsuarioRegistroPredeterminado);
+            this.EntradaContraseña.SendKeys(this.ContraseñaPredeterminada);
+            this.EntradaConfirmarContraseña.SendKeys(this.ContraseñaPredeterminada);
+            this.BotonRegistrarse.Click();
+        }
+
+        public void CerrarSesion()
+        {
+            this.BotonCerrarSesion.Click();
+        }
+
+        public void EliminarUsuarioRegistrado()
+        {
+            HandlerGenerico handler = new HandlerGenerico();
+            string consulta = $"Delete from AspNetUsers where email = {this.CorreoPredeterminado}";
+            handler.EnviarConsultaGenerica(consulta);
+        }
+    }
+}
---
+++ src/PI/functional_tests/HandlerGenerico.cs
@@ -0,0 +1,23 @@
+﻿using PI.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsResources
+{
+    public class HandlerGenerico : Handler
+    {
+        public void EnviarConsultaGenerica(string consulta)
+        {
+            base.enviarConsultaVoid(consulta);
+        }
+
+        public DataTable CrearTablaConsultaGenerico(string consulta)
+        {
+            return base.CrearTablaConsulta(consulta);
+        }
+    }
+}
