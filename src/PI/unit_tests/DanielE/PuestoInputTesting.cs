3821d5efbd6b31dea202b65e1fa6f235883845db
DanielEscobar19 <danielonchio@gmail.com>
2022-11-17 21:50:37 -0600

    Platnilla vacia de test. Se debe revisar ocmo usar bUnit para probar compnentes razor.

---
+++ src/PI/unit_tests/DanielE/PuestoInputTesting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PI.Views.Shared.Components;
+
+namespace unit_tests.DanielE
+{
+    [TestClass]
+    public class PuestoInputTesting
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+
+        [TestMethod]
+        public void PuestoInputTest()
+        {
+
+        }
+    }
+}
