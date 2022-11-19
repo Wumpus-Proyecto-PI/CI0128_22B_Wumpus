c482d5d16ec2a9bbae740a5b59ca0049458b7615
gabriel.bonillarivera <gabriel.bonillarivera@ucr.ac.cr>
2022-11-19 17:46:29 -0600

    Agrego métiodo IngresarEgreso

--- src/PI/PI/Handlers/FlujoDeCajaHandler.cs
+++ src/PI/PI/Handlers/FlujoDeCajaHandler.cs
@@ -10,7 +10,7 @@ namespace PI.Handlers
 
         public List<EgresoModel> ObtenerEgresosMes(string NombreMes, DateTime FechaAnalisis) 
         {
-            string consulta = "EXEC ObtenerEgresosMes @fechAnalisis ='" + FechaAnalisis +"', @nombreMes ='" + NombreMes +"';";
+            string consulta = "EXEC ObtenerEgresosMes @fechaAnalisis ='" + FechaAnalisis +"', @nombreMes ='" + NombreMes +"';";
             DataTable tablaResultado = CrearTablaConsulta(consulta);
             List<EgresoModel> egresos = new List<EgresoModel>();
             foreach (DataRow columna in tablaResultado.Rows)
@@ -19,7 +19,7 @@ namespace PI.Handlers
                 new EgresoModel
                 {
                     FechaAnalisis = Convert.ToDateTime(columna["fechaAnalisis"]),
-                    Tipo = Convert.ToBoolean(columna["tipo"]),
+                    Tipo = Convert.ToInt32(columna["tipo"]),
                     Monto = Convert.ToDecimal(columna["monto"]),
                     Mes = Convert.ToString(columna["mes"])
                 }
@@ -29,7 +29,7 @@ namespace PI.Handlers
         }
         public List<IngresoModel> ObtenerIngresosMes(string NombreMes, DateTime FechaAnalisis)
         {
-            string consulta = "EXEC ObtenerEgresosMes @fechAnalisis ='" + FechaAnalisis + "', @nombreMes ='" + NombreMes + "';";
+            string consulta = "EXEC ObtenerEgresosMes @fechaAnalisis ='" + FechaAnalisis + "', @nombreMes ='" + NombreMes + "';";
             DataTable tablaResultado = CrearTablaConsulta(consulta);
             List<IngresoModel> ingresos = new List<IngresoModel>();
             foreach (DataRow columna in tablaResultado.Rows)
@@ -38,13 +38,20 @@ namespace PI.Handlers
                 new IngresoModel
                 {
                     FechaAnalisis = Convert.ToDateTime(columna["fechaAnalisis"]),
-                    Tipo = Convert.ToBoolean(columna["tipo"]),
+                    Tipo = Convert.ToInt32(columna["tipo"]),
                     Monto = Convert.ToDecimal(columna["monto"]),
                     Mes = Convert.ToString(columna["mes"])
                 }
                 );
             }
             return ingresos;
+        }
+
+        public void IngresarEgreso(IngresoModel Ingreso) 
+        {
+            string consulta = "EXEC AgregarEgresoMes @fechaAnalisis='" + Ingreso.FechaAnalisis
+                + "', @nombreMes ='" + Ingreso.Mes + "', @tipo=" + Ingreso.Tipo + ", @monto=" + Ingreso.Monto;
+            enviarConsultaVoid(consulta);
         }
 
 
