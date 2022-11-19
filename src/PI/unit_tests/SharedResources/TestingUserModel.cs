48fb79a590a62636cfa362a3354fbcb65c2a5680
DanielEscobar19 <danielonchio@gmail.com>
2022-11-18 21:15:57 -0600

    Model de usuario de testing. Tenemos un usuario default de testing llamado wumnpustest@gmail.com. Con esta clase teenmos acceso a su has code y a su email.

---
+++ src/PI/unit_tests/SharedResources/TestingUserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_tests.SharedResources
+{
+    public static class TestingUserModel
+    {
+        public static string UserId { get; } = "e690ef97-31c4-4064-bede-93aeedaf6857";
+        public static string UserEmail { get; } = "wumpustest@gmail.com";
+    }
+}
