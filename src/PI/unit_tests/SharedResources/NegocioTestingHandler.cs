16c83ae2d2186a8a647c7efcca12993b9677b37c
DanielEscobar19 <danielonchio@gmail.com>
2022-11-18 21:14:14 -0600

    Creacion de handler de testing de negocio. Este permite crear un negocio de testing y elimnar el mismo. Esto se usa en los TestInitialize y en los TestCleanup.

---
+++ src/PI/unit_tests/SharedResources/NegocioTestingHandler.cs
@@ -0,0 +1,34 @@
+﻿using PI.Handlers;
+using PI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using static ClosedXML.Excel.XLPredefinedFormat;
+
+namespace unit_tests.SharedResources
+{
+    public class NegocioTestingHandler : NegocioHandler
+    {
+        public NegocioModel? NegocioFicticio { get; private set; } = null;
+        public string NombreNegocioFicticio { get; } = "Negocio ficticio";
+
+        public NegocioModel IngresarNegocioFicticio(string idUsuario, string tipoNegocio = "Emprendimiento")
+        {
+            NegocioFicticio = base.IngresarNegocio(NombreNegocioFicticio, tipoNegocio, idUsuario);
+            return NegocioFicticio;
+        }
+
+        public void EliminarNegocioFicticio()
+        {
+            try
+            {
+                base.EliminarNegocio(NegocioFicticio.ID.ToString());
+            } catch(NullReferenceException) { 
+                throw new NullReferenceException("El negocio actual es nulo.");
+            }
+        }
+    }
+}
