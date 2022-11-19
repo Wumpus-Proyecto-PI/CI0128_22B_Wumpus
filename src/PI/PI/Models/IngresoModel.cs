70d145cbdf635252dde14ec1ce69a95115bddd07
gabriel.bonillarivera <gabriel.bonillarivera@ucr.ac.cr>
2022-11-19 17:03:00 -0600

    Agrego IngresoModel

---
+++ src/PI/PI/Models/IngresoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PI.Models
+{
+    public class IngresoModel
+    {
+        public string Mes { get; set; } = "";
+        public bool Tipo { get; set; } = false;
+        public decimal Monto { get; set; } = 0M;
+        public DateTime FechaAnalisis { get; set; }
+
+    }
+}
