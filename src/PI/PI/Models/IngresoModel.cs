52b7acaf2870c1ee0f9ccd6d17b349f0c3d4e75a
gabriel.bonillarivera <gabriel.bonillarivera@ucr.ac.cr>
2022-11-19 19:11:23 -0600

    Cambio Egreso/Ingreso tipo a String en Modelos

--- src/PI/PI/Models/IngresoModel.cs
+++ src/PI/PI/Models/IngresoModel.cs
@@ -3,7 +3,7 @@
     public class IngresoModel
     {
         public string Mes { get; set; } = "";
-        public int Tipo { get; set; } = 0;
+        public string Tipo { get; set; } = "";
         public decimal Monto { get; set; } = 0M;
         public DateTime FechaAnalisis { get; set; }
 
