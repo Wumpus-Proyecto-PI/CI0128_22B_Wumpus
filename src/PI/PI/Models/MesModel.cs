43ec689213bd8d3992b49c3473b556040cf8ce23
Chris24091 <christopher.perez.cr@gmail.com>
2022-11-23 18:57:40 -0600

    Agregando atributos a modelo. Se agregan los atributos al modelo del mes segun las columnas de su tabla en la base de datos.

--- src/PI/PI/Models/MesModel.cs
+++ src/PI/PI/Models/MesModel.cs
@@ -2,5 +2,12 @@
 {
     public class MesModel
     {
+        public string NombreMes { get; set; }
+
+        public string FechaAnalisis { get; set; }   
+
+        public decimal SalarioPorMes { get; set; }
+
+        public decimal InversionPorMes { get; set; }
     }
 }
