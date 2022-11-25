c21caa007444df72a9d22b2f5b5304836a6ed015
Chris24091 <christopher.perez.cr@gmail.com>
2022-11-24 23:09:05 -0600

    Se agrega mock de pantalla. Se agrega la pantalla del progreso del analisis al proyecto de tests funcionales.

---
+++ src/PI/functional_tests/Pages/ProgresoAnalisisPage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functional_tests.Pages
+{
+    public class ProgresoAnalisisPage
+    {
+        private IWebDriver Driver; 
+
+        public IWebElement TarjetaEstructuraOrg
+        {
+            get
+            {
+                return Driver.FindElement(By.ClassName("titulo-carta"));
+            }
+        }
+
+        public void IngresarEstructuraOrg()
+        {
+            if (TarjetaEstructuraOrg.Text == "Estructura Organizativa")
+            {
+                TarjetaEstructuraOrg.Click();
+            }
+            
+        }
+
+        public ProgresoAnalisisPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+    }
+}
