bebd3a7d5373556b50dceb36fa98990b38ae18e3
WILMER.ARAYA <wilmer.araya@ecci.ucr.ac.cr>
2022-11-26 23:31:59 -0600

    Agrego clase para tests funcionales

---
+++ src/PI/functional_tests/Wilmer/WilmerTests.cs
@@ -0,0 +1,17 @@
+﻿using functional_tests.Pages;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functional_tests.Wilmer
+{
+    [TestClass]
+    public class WilmerTests
+    {
+       
+    }
+}
