749c913c2071c01b0c2f1438376a85dba5c3473f
DanielEscobar19 <danielonchio@gmail.com>
2022-11-28 13:12:19 -0600

    Code review. Simplificacion de strings de consulta.

--- src/PI/PI/Handlers/EstructuraOrgHandler.cs
+++ src/PI/PI/Handlers/EstructuraOrgHandler.cs
@@ -45,7 +45,7 @@ namespace PI.Handlers
 
                 // realizamos la consulta
                 // este método es heredado del padre y permite enviar consultas de actualización, borrado e inserción   
-                enviarConsulta(insert);
+                base.enviarConsulta(insert);
             }
         }
 
@@ -62,7 +62,7 @@ namespace PI.Handlers
                 + "nombre='" + nombrePuesto + "' and "
                 + "fechaAnalisis='" + fechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") + "'";
             // método heredado de la clase padre para realizar consultas de tipo SELECT.
-            DataTable tablaResultadoPuestos = CrearTablaConsulta(consulta);
+            DataTable tablaResultadoPuestos = base.CrearTablaConsulta(consulta);
             
             // revisamos si la consutla retorno algo
             if (tablaResultadoPuestos.Rows.Count > 0 && tablaResultadoPuestos.Rows[0].IsNull("nombre") == false)
@@ -78,26 +78,17 @@ namespace PI.Handlers
         // Método que permite actualizar un puesto ya existente
         // Recibe el nombre el puesto anterior y el modelo a insertar
         // Retorna true si se se dio un error en la consulta 
-        public bool ActualizarPuesto(string nombrePuesto, PuestoModel puestoInsertar)
+        public void ActualizarPuesto(string nombrePuesto, PuestoModel puestoInsertar)
         {
-            bool error = false;
-
             // consulta sql con la cual actualizamos el puesto. Aquí támbien casteamos los decimales según si tiene punto o decimal
-            string update = "DECLARE @salarioTemp varchar(20), @beneficiosTemp varchar(20) SET @salarioTemp = '" + puestoInsertar.SalarioBruto.ToString()
-                + "' SET @salarioTemp = REPLACE(@salarioTemp, ',', '.') SET @beneficiosTemp = '" +puestoInsertar.Beneficios.ToString()
-                + "' SET @beneficiosTemp = REPLACE(@beneficiosTemp, ',' , '.') UPDATE PUESTO SET "
+            string update = "UPDATE PUESTO SET "
                 + "nombre='" + puestoInsertar.Nombre + "', "
                 + "cantidadPlazas='" + puestoInsertar.Plazas.ToString() + "', "
-                + "salarioBruto= dbo.convertTOdecimal ( @salarioTemp), beneficios= dbo.ConvertToDecimal(@beneficiosTemp)"
-                + "WHERE "
-                + "nombre='" + nombrePuesto + "' and "
-                + "fechaAnalisis='" + puestoInsertar.FechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") + "';";
-            int filasAfectadas = enviarConsulta(update);
-            if (filasAfectadas < 0)
-            {
-                error = true;
-            }
-            return error;
+                + "salarioBruto= " + puestoInsertar.SalarioBruto.ToString().Replace(",", ".") + ", "
+                + "beneficios= " + puestoInsertar.Beneficios.ToString().Replace(",", ".") + " "
+                + "WHERE nombre='" + nombrePuesto + "' and "
+                + "fechaAnalisis='" + puestoInsertar.FechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") + "'";
+            base.enviarConsulta(update);
         }
 
 
@@ -109,7 +100,7 @@ namespace PI.Handlers
             // consulta sql que llama procedure que retorna todos los puestos de una análisis
             string consulta = "execute ObtenerPuestos @fechaAnalisis = '" + fechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") + "'";
             // método de la clase padre para realizar consultas
-            DataTable tablaResultadoPuestos = CrearTablaConsulta(consulta);
+            DataTable tablaResultadoPuestos = base.CrearTablaConsulta(consulta);
 
             // lista para almacenar los puestos
             List<PuestoModel> puestos = new List<PuestoModel>();
@@ -132,12 +123,12 @@ namespace PI.Handlers
         }
 
         // Método para eliminar un puesto de un análisis
-        public int EliminarPuesto(PuestoModel puestoELiminar)
+        public void EliminarPuesto(PuestoModel puestoELiminar)
         {
             string delete = "DELETE FROM PUESTO WHERE fechaAnalisis='" + 
                 puestoELiminar.FechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") + "' " 
                 + "and nombre='" + puestoELiminar.Nombre + "';";
-            return enviarConsulta(delete);
+            base.enviarConsulta(delete);
         }
     }
 }
