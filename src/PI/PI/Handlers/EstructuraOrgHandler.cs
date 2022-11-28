4641891f341ba2c9f59f060271615b875ded4d4c
DanielEscobar19 <danielonchio@gmail.com>
2022-11-28 13:04:27 -0600

    Code review. Se elimina codigo muerto

--- src/PI/PI/Handlers/EstructuraOrgHandler.cs
+++ src/PI/PI/Handlers/EstructuraOrgHandler.cs
@@ -24,10 +24,8 @@ namespace PI.Handlers
         // EL string nombrePuesto es el nombre antes de modificaciones. Esto se usa para actualizaciones.
         // El modelo es para acceder a todos los datos de tal puesto
         // Este método retorna la cantidad de filas actualizadas
-        public int InsertarPuesto(string nombrePuesto, PuestoModel puesotAInsertar)
+        public void InsertarPuesto(string nombrePuesto, PuestoModel puesotAInsertar)
         {
-            // cuenta las filas afectadas en la base de datos
-            int filasAfectadas = 0;
 
             // primero se revisa si ya existe el puesto en la base
             if (ExistePuestoEnBase(nombrePuesto, puesotAInsertar.FechaAnalisis))
@@ -38,21 +36,17 @@ namespace PI.Handlers
             {
                 // en caso de no existir lo insertamos como un nuevo puesto
                 // en estaconsulta de sql convertimos los decimales según si es con coma o punto para evitar errores
-                string insert = "DECLARE @salarioTemp varchar(20), @beneficiosTemp varchar(20) SET @salarioTemp = '"+ puesotAInsertar.SalarioBruto.ToString() 
-                + "' SET @salarioTemp = REPLACE(@salarioTemp, ',', '.')  SET @beneficiosTemp = '"+puesotAInsertar.Beneficios.ToString()
-                + "' SET @beneficiosTemp = REPLACE(@beneficiosTemp, ',', '.') INSERT INTO PUESTO values ("
-                + "'" + puesotAInsertar.Nombre + "', "
-                + "'" + puesotAInsertar.FechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") + "', "
+                string insert = "INSERT INTO PUESTO values ('"
+                + puesotAInsertar.Nombre + "', '"
+                + puesotAInsertar.FechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") + "', "
                 + puesotAInsertar.Plazas.ToString() + ", "
-                + "dbo.convertTOdecimal ( @salarioTemp), dbo.ConvertToDecimal(@beneficiosTemp))";
-
-                Console.WriteLine(insert);
+                + puesotAInsertar.SalarioBruto.ToString().Replace(",", ".") + ", "
+                + puesotAInsertar.Beneficios.ToString().Replace(",", ".") + ")";
 
                 // realizamos la consulta
                 // este método es heredado del padre y permite enviar consultas de actualización, borrado e inserción   
-                filasAfectadas = enviarConsulta(insert);
+                enviarConsulta(insert);
             }
-            return filasAfectadas;
         }
 
         // Método que permite revisar si ya existe un puesto en la base
@@ -124,7 +118,6 @@ namespace PI.Handlers
             {
                 // ingresamos todos los datos de cada puesto
                 PuestoModel puesto = new PuestoModel();
-                Console.WriteLine(puesto.Nombre);
                 puesto.Nombre = Convert.ToString(fila["nombre"]);
                 puesto.Plazas = Convert.ToInt32(fila["cantidadPlazas"]);
                 puesto.SalarioBruto = Convert.ToDecimal(fila["salarioBruto"]);
@@ -146,115 +139,5 @@ namespace PI.Handlers
                 + "and nombre='" + puestoELiminar.Nombre + "';";
             return enviarConsulta(delete);
         }
-
-       
-
-      
-
-
-        /* 
-         * Importante!
-         * Métodos para obtener los puestos con estructura no son utilizados en el primer sprint 
-         * Los métodos siguientes cargan los puestos tomando encuenta la jerarquía
-         */
-
-        /*
-         * Estos métodos se deben revisar y mejorar para sigueintes sprints
-         */
-
-        // Método que obtiene los puestos de un análisis tomando en cuenta la jerarquía
-        // Recibe la fecha del análisis de donde extraer los puestos
-        // Retorna el puesto raíz que contiene el restos de los peustos como subordinados
-        public PuestoModel ObtenerEstructuraOrg(DateOnly fechaAnalisis)
-        {
-            // extraer los puestos
-            // nombrePuesto es la llave primaria de puesto
-            string fechaAnalisisStr = fechaAnalisis.ToString("yyyy-mm-dd");
-            string consulta = "SELECT TOP 1 nombre FROM PUESTO WHERE fechaAnalisis='" + fechaAnalisisStr + "'";
-            DataTable tablaResultadoPuestos = CrearTablaConsulta(consulta);
-            var filas = tablaResultadoPuestos.Rows;
-            // puesto raiz que contiene los otros puestos como subordinados
-            // ObtenerUnPuesto es metodo recursivo que va a agregar todos los subordinados de la estructura
-            PuestoModel raizEstructura = ObtenerUnPuesto(Convert.ToString(filas[0]["nombre"]), fechaAnalisisStr);
-
-            return raizEstructura;
-        }
-
-        // Método que retorna un solo puestos y sus subordinados de forma recursiva
-        // Recibe el nombre del puesto, la fecha del análisis y un booleano que indica si se desea cargar los subordinados del puesto
-        // Retorna un puesto con sus subordinados si se indica
-        public PuestoModel ObtenerUnPuesto(string nombrePuesto, string fechaAnalisis, bool extraerSubordinados = true)
-        {
-            // extraeamos la lista de puestos 
-            string consulta = "SELECT TOP 1 * FROM PUESTO WHERE"
-                + "'fechaAnalisis='" + fechaAnalisis + "' and "
-                + "nombre='" + nombrePuesto + "'";
-            DataTable tablaResultadoPuestos = CrearTablaConsulta(consulta);
-            var filas = tablaResultadoPuestos.Rows;
-
-            if (tablaResultadoPuestos == null && tablaResultadoPuestos.Rows.Count <= 0)
-            {
-                return null;
-            }
-
-            PuestoModel puestoActual = new PuestoModel
-            {
-                Nombre = nombrePuesto,
-                Plazas = Convert.ToInt16(filas[0]["plazasPorPuesto"]),
-                SalarioBruto = Convert.ToDecimal(filas[0]["salarioBruto"]),
-            };
-            // puestoActual.Beneficios = ObtenerBeneficios(puestoActual.Nombre, fechaAnalisis);
-
-            // si se indica que desean extraer tambien los subordinados
-            // si esta en falso se evita un query muy grande que puede tomar tiempo
-            if (extraerSubordinados)
-            {
-                // extraer los puesto subordinados
-                consulta = "SELECT * FROM ES_EMPLEADO_DE as E join Puesto as P on "
-                    + "E.nombreEmpleado = P.nombre"
-                    + "WHERE E.nombreJefe='" + puestoActual.Nombre + "'"
-                    + "AND P.fechaAnalisis='" + fechaAnalisis + "'"
-                    + "AND E.fechaEmpleado='" + fechaAnalisis + "'"
-                    + "AND E.fechaJefe='" + fechaAnalisis + "'";
-
-                DataTable tablaSubordinados = CrearTablaConsulta(consulta);
-                if (tablaSubordinados != null && tablaSubordinados.Rows.Count > 0)
-                {
-                    foreach (DataRow fila in tablaSubordinados.Rows)
-                    {
-                        PuestoModel subordinado = ObtenerUnPuesto(Convert.ToString(fila["nombre"]), fechaAnalisis, true);
-
-                        if (subordinado != null)
-                        {
-                            puestoActual.Subordinados.Add(subordinado);
-                        }
-                    }
-                }
-                else
-                {
-                    return null;
-                }
-            }
-
-            return puestoActual;
-        }
-
-        public decimal obtenerMontoTotalSalarios(DateTime fechaAnalisis)
-        {
-            decimal total = 0.0m;
-
-            string consulta = "SELECT SUM(salarioBruto) as total" +
-                             " FROM PUESTO" +
-                             " WHERE fechaAnalisis = '" + fechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") + "'";
-
-            DataTable tablaResultado = CrearTablaConsulta(consulta);
-            if (!tablaResultado.Rows[0].IsNull("total"))
-            {
-                total = Convert.ToDecimal(tablaResultado.Rows[0]["total"]);
-            }
-
-            return total;
-        }
-
     }
 }
