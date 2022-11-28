6c2413079e76ce55c0e9dc98c267c1cdf94b3433
Chris24091 <christopher.perez.cr@gmail.com>
2022-11-28 15:07:26 -0600

    Se agrega el archivo en el que se realizaran las pruebas unitarias para los gastos variables.

---
+++ src/PI/unit_tests/Chris/GastoVariableTesting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PI.Handlers;
+using PI.Models;
+using unit_tests.SharedResources;
+
+namespace unit_tests.Chris
+{
+    public class GastoVariableTesting
+    {
+    }
+}
