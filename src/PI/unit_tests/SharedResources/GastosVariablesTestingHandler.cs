75ccfbba60b24131cc5874cdc09b48dcdc412929
Chris24091 <christopher.perez.cr@gmail.com>
2022-11-28 15:24:39 -0600

    Se agrega metodo. Se agrega el metodo para obtener los productos de la bas de datos.

--- src/PI/unit_tests/SharedResources/GastosVariablesTestingHandler.cs
+++ src/PI/unit_tests/SharedResources/GastosVariablesTestingHandler.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PI.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,29 @@ namespace unit_tests.SharedResources
 {
     public class GastosVariablesTestingHandler : HandlerGenerico
     {
+        public List<ProductoModel> leerProductosDeBase(DateTime fechaAnalisis)
+        {
+            List<ProductoModel> productos = new List<ProductoModel>();
+
+            string consulta = "EXEC ObtenerProductos @fechaAnalisis='" + fechaAnalisis.ToString("yyyy-MM-dd HH:mm:ss.fff") + "'";
+
+            DataTable tablaResultado = base.CrearTablaConsultaGenerico(consulta);
+            foreach (DataRow columna in tablaResultado.Rows)
+            {
+                productos.Add(
+                new ProductoModel
+                {
+                    Nombre = Convert.ToString(columna["nombre"]),
+                    FechaAnalisis = Convert.ToDateTime(columna["fechaAnalisis"]),
+                    Lote = Convert.ToInt32(columna["lote"]),
+                    PorcentajeDeVentas = Convert.ToDecimal(columna["porcentajeDeVentas"]),
+                    Precio = Convert.ToDecimal(columna["precio"]),
+                    CostoVariable = Convert.ToDecimal(columna["costoVariable"]),
+                    ComisionDeVentas = Convert.ToDecimal(columna["comisionDeVentas"])
+                }
+                );
+            }
+            return productos;
+        }
     }
 }
