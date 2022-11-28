3793c79470117c502165f3ecb60aeddb5a27eba9
Chris24091 <christopher.perez.cr@gmail.com>
2022-11-28 15:42:44 -0600

    Se agregan atributos. Se crean los atributos necesarios para poder mockear las pruebas de los gastos variables.

--- src/PI/unit_tests/Chris/GastoVariableTesting.cs
+++ src/PI/unit_tests/Chris/GastoVariableTesting.cs
@@ -9,7 +9,21 @@ using unit_tests.SharedResources;
 
 namespace unit_tests.Chris
 {
-    public class GastoVariableTesting
+    public class GastoVariableTesting : HandlerGenerico
     {
+        // handler de testing que permite crear un negocio de pruebas
+        private NegocioTestingHandler? NegocioTestingHandler = null;
+
+        // handler de analisis que nos permite obtener analisis del negocio
+        private AnalisisHandler? AnalisisHandler = null;
+
+        // negocio ficticio creado para la prueba
+        private NegocioModel? NegocioFicticio = null;
+
+        // analisis ficticio creado para la prueba
+        private AnalisisModel? AnalisisFicticio = null;
+
+        // handler de testing usado para manejar la creacion de puestos semilla
+        GastosVariablesTestingHandler? GastoVariableTestingHandler = null;
     }
 }
