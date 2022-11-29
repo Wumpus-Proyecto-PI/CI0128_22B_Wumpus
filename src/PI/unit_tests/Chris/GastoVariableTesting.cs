eb732f6839245b5c687eac8a4df4ecdd9de3da82
Chris24091 <christopher.perez.cr@gmail.com>
2022-11-28 18:02:44 -0600

    Se agrega test. Se agrega el metodo del test que prueba la eliminacion de un producto no existente en la base de datos.

--- src/PI/unit_tests/Chris/GastoVariableTesting.cs
+++ src/PI/unit_tests/Chris/GastoVariableTesting.cs
@@ -362,8 +362,33 @@ namespace unit_tests.Chris
         public void EliminarGastoVariable_NoExistente()
         {
             //arrange
+            ProductoModel producto = new ProductoModel()
+            {
+                Nombre = "producto-test",
+                FechaAnalisis = AnalisisFicticio.FechaCreacion,
+                Lote = 1,
+                CostoVariable = 1,
+                ComisionDeVentas = 1,
+                PorcentajeDeVentas = 1,
+                Precio = 1
+            };
+
+            // creamos handler con el metodo que deseamos probar
+            ProductoHandler productoHandler = new();
+
+            // No se inserta ya que lo que se va a probar es cuando no esta en la base
+
+            List<ProductoModel> productosPreEliminacion = GastosVariablesTestingHandler.leerProductosDeBase(AnalisisFicticio.FechaCreacion);
+            int cantidadAntesDeBorrar = productosPreEliminacion.Count();
+
             //action
+            productoHandler.EliminarProducto(producto);
+
+            List<ProductoModel> productosPostEliminado = GastosVariablesTestingHandler.leerProductosDeBase(AnalisisFicticio.FechaCreacion);
+            int cantidadDespuesDeBorrar = productosPostEliminado.Count();
+
             //assert
+            Assert.AreEqual(cantidadAntesDeBorrar, cantidadDespuesDeBorrar);
         }
     }
 }
