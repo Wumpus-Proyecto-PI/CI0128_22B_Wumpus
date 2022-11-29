d0351e491b3c4cc32a2d2fbc39d69ca8ae2ec023
DanielEscobar19 <danielonchio@gmail.com>
2022-11-29 01:00:51 -0600

    Se agrega usuario de testing para unit tests.

--- src/PI/unit_tests/SharedResources/TestingUserModel.cs
+++ src/PI/unit_tests/SharedResources/TestingUserModel.cs
@@ -12,7 +12,7 @@ namespace unit_tests.SharedResources
     {
         // hashcode que se asigan por identity al crear un usuario
         // este se usa para unir las tablas de negocio y de usuario de identity
-        public static string UserId { get; } = "81727ffe-84fc-4263-b7e4-e763664968d9";
+        public static string UserId { get; } = "473c213a-682c-4837-9497-244f8e044dc6";
 
         // correo del usuario de etsting que se creo
         public static string UserCorreo { get; } = "wumpustest@gmail.com";
