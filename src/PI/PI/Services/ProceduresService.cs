fece9a8c65092e7e992b44a31d97ac00d6380129
DanielEscobar19 <danielonchio@gmail.com>
2023-03-15 18:33:00 -0600

    Se agrega servic o e procedures

---
+++ src/PI/PI/Services/ProceduresService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PI.EntityModels;
+
+namespace PI.Services
+{
+    public class ProceduresServices
+    {
+        private DataBaseContext? Contexto = null;
+
+        public ProceduresServices(DataBaseContext contexto)
+        {
+            Contexto = contexto;
+        }
+
+        ~ProceduresServices()
+        {
+            Contexto = null;
+        }
+
+        public async Task<List<Ingreso>> ObtenerIngresosAsync(DateTime fechaAnalisis)
+        {
+            return await Contexto.Ingresos.AsNoTracking().Where(ingreso => ingreso.FechaAnalisis == fechaAnalisis).ToListAsync();
+        }
+
+        public async Task<List<Egreso>> ObtenerEgresosAsync(DateTime fechaAnalisis)
+        {
+            return await Contexto.Egresos.AsNoTracking().Where(ingreso => ingreso.FechaAnalisis == fechaAnalisis).ToListAsync();
+        }
+
+        public async Task<int> ObtenerTipoAnalisisAsync(DateTime fechaAnalisis)
+        {
+            return (await Contexto.Configuracion.FindAsync(fechaAnalisis)).TipoNegocio;
+        }
+    }
+}
