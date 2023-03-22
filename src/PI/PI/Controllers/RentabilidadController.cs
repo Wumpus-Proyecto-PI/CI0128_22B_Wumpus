e21a5a6afaaba8056e678e0d50a4dd48a79eac89
WILMER.ARAYA <wilmer.araya@ecci.ucr.ac.cr>
2023-03-21 21:55:28 -0600

    Cambio controlador de Analisis de rentabilidad

--- src/PI/PI/Controllers/RentabilidadController.cs
+++ src/PI/PI/Controllers/RentabilidadController.cs
@@ -1,32 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using PI.Handlers;
-using PI.Models;
+using PI.EntityModels;
+using PI.EntityHandlers;
 using PI.Services;
 
 namespace PI.Controllers
 {
     public class RentabilidadController : ManejadorUsuariosController
     {
+
+        private ProductoHandler? ProductoHandler = null;
+        private AnalisisHandler? AnalisisHandler = null;
+        private NegocioHandler? NegocioHandler = null;
+        private GastoFijoHandler? GastoFijoHandler = null;
+
+        public RentabilidadController(
+            NegocioHandler negocioHandler, ProductoHandler productoHandler,
+            AnalisisHandler analisisHandler, GastoFijoHandler gastoFijoHandler)
+        {
+            ProductoHandler = productoHandler;
+            AnalisisHandler = analisisHandler;
+            NegocioHandler = negocioHandler;
+            GastoFijoHandler = gastoFijoHandler;
+        }
+
         // // Retorna una lista con todos los modelos de producto existentes y el título del paso.
-        public IActionResult Index(string fecha)
+        public async Task<IActionResult> Index(string fecha)
         {
             DateTime fechaConversion = DateTime.ParseExact(fecha, "yyyy-MM-dd HH:mm:ss.fff", null);
             ViewBag.fechaAnalisis = fechaConversion;
 
-            AnalisisHandler analisisHandler = new AnalisisHandler();
-            ProductoHandler productoHandler = new ProductoHandler();
-            List<ProductoModel> productos = productoHandler.ObtenerProductos(fechaConversion);
-            AnalisisModel model = analisisHandler.ObtenerUnAnalisis(ViewBag.fechaAnalisis);
+
+            List<Producto> productos = await ProductoHandler.ObtenerProductosAsync(fechaConversion);
+            Analisis model = await AnalisisHandler.ObtenerUnAnalisis(ViewBag.fechaAnalisis);
             ViewBag.AnalisisActual = model;
 
-            GastoFijoHandler gastoFijoHandler = new GastoFijoHandler();
-            ViewBag.MontoGastosFijosMensuales = AnalisisRentabilidadService.CalcularGastosFijosTotalesMensuales(gastoFijoHandler.obtenerTotalAnual(fechaConversion));
+            decimal totalAnualGastosFijos = await GastoFijoHandler.ObtenerTotalAnualAsync(fechaConversion);
+            ViewBag.MontoGastosFijosMensuales = AnalisisRentabilidadService.CalcularGastosFijosTotalesMensuales(totalAnualGastosFijos);
             // para que se muestre el boton de volver al analisis
             ViewBag.BotonRetorno = "Progreso";
 
             // asignamos el nombre del negocio en la vista
             // este se extrae de la base de datos con la fecha del análisis
-            ViewData["NombreNegocio"] = gastoFijoHandler.obtenerNombreNegocio(fechaConversion);
+            ViewData["NombreNegocio"] = await NegocioHandler.ObtenerNombreNegocioAsync(fechaConversion);
 
             ViewData["TituloPaso"] = "Análisis de rentabilidad";
             // se asigna el titulo en la pestaña del cliente
