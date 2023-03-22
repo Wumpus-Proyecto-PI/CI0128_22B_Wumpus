a54dd3d309a411adda2183f2cbae05544e9e9055
gabrielb19 <gabr.cr1910@gmail.com>
2023-03-21 21:40:24 -0600

    Agrego métodos de operaciones a EntityHandler

--- src/PI/PI/EntityHandlers/EntityHandler.cs
+++ src/PI/PI/EntityHandlers/EntityHandler.cs
@@ -1,12 +1,16 @@
-﻿using PI.EntityModels;
+﻿using DocumentFormat.OpenXml.InkML;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.EntityFrameworkCore;
+using PI.EntityModels;
 
 namespace PI.EntityHandlers
 {
-    public abstract class EntityHandler: IDisposable
+    public abstract class EntityHandler : IDisposable
     {
         protected DataBaseContext? Contexto = null;
 
-        public EntityHandler(DataBaseContext contexto) {
+        public EntityHandler(DataBaseContext contexto)
+        {
             Contexto = contexto;
         }
 
@@ -19,6 +23,40 @@ namespace PI.EntityHandlers
         {
             Contexto?.Dispose();
             Contexto = null;
+        }
+
+        public async Task<string> ObtenerNombreDeNegocio(DateTime fechaAnalisis)
+        {
+            // string a retornar con el nombre del negocio
+            string nombreNegocio = await Contexto.Negocios
+                .Join(Contexto.Analisis,
+                    a => a.Id,
+                    b => b.IdNegocio,
+                    (a, b) => new { Negocios = a, Analisis = b })
+                .Where(x => x.Analisis.FechaCreacion == fechaAnalisis)
+                .Select(x => x.Negocios.Nombre)
+                .Distinct().FirstOrDefaultAsync();
+
+
+            if (string.IsNullOrEmpty(nombreNegocio))
+            {
+                // existe el caso de que un negocio no tenga nombre en nuestro producto
+                nombreNegocio = "Sin nombre";
+            }
+
+            return nombreNegocio;
+        }
+
+        public async Task<Negocio> ObtenerNegocioDeAnalisis(DateTime fechaAnalisis)
+        {
+            Analisis analisis = await Contexto.Analisis.Where(x => x.FechaCreacion == fechaAnalisis).FirstOrDefaultAsync();
+            Negocio negocio = await Contexto.Negocios.Where(x => x.Id == analisis.IdNegocio).FirstOrDefaultAsync();
+            return negocio;
+        }
+
+        public async Task<string> ObtenerNombreDeNegocio(int idNegocio)
+        {
+            return await Contexto.Negocios.Where(x => x.Id == idNegocio).Select(x => x.Nombre).FirstOrDefaultAsync();
         }
     }
 }
